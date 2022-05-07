df41bafe5e9556e018e398ca30eecdbcd33fb03d
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-07 09:34:53 +0000

    Add varibles for rpsls to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ function myFunction() {
       links.style.display = "block";
     }
 } // code for hamburger menu
+
+// rpsls js
+let computerChoiceDisplay = document.getElementById("computer-choice");
+let userChoiceDisplay = document.getElementById("user-choice");
+let resultDisplay = document.getElementById("rpsls-result");
+let scoreDisplay = document.getElementById("rpsls-score")
+let userChoice;
+let computerChoice;
+let getResults;
