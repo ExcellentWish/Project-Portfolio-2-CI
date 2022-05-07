f82640d0af902baef6cbfeb08330c41e8067fb74
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-07 15:53:12 +0000

    add whack a mole js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,3 @@ function myFunction() {
 } // code for hamburger menu
 
 
-// whack a mole
-const squares = document.getElementsByClassName('square');
-const mole = document.getElementsByClassName('mole');
-let timeLeft = document.getElementById('time-left');
-let scoreWhackaMole = document.getElementById('whack-a-mole-score') 
---
+++ assets/js/whack-a-mole.js
@@ -0,0 +1,4 @@
+const squares = document.getElementsByClassName('square');
+const mole = document.getElementsByClassName('mole');
+let timeLeft = document.getElementById('time-left');
+let scoreWhackaMole = document.getElementById('whack-a-mole-score') 
