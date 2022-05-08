a22893e0ecee393733e7ffb693377c1dabe41e68
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-08 17:30:04 +0000

    Add movemole around grid functions

--- assets/js/whack-a-mole.js
+++ assets/js/whack-a-mole.js
@@ -6,3 +6,18 @@ let result = 0;
 let hitPosition;
 let currentTime = 60;
 let timerId = null;
+
+function randomSquare(){
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+    
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+    hitPosition = randomSquare.id;
+}
+function moveMole(){
+   timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole();
