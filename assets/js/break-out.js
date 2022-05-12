c708fe633e13af74ca360125651c1fabcfc8a4c7
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-12 09:55:36 +0000

    Add create block funtion in JS

--- assets/js/break-out.js
+++ assets/js/break-out.js
@@ -1,3 +1,27 @@
 const scoreDisplay = document.querySelector('#score');
 let score = 0;
 const grid = document.querySelector('.break-out-grid');
+const blockWidth = 100;
+const blockHeight = 20;
+
+class Block{
+    constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topLeft = [xAxis, yAxis + blockHeight]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+
+
+function addBlocks(){
+    for(let i = 0; i < blocks.length; i++){
+   
+       const block = document.createElement('div');
+       block.classList.add('block');
+       block.style.left = blocks[i].bottomLeft[0] + 'px';
+       block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+       grid.appendChild(block);
+     }
+   }
+   addBlocks();
