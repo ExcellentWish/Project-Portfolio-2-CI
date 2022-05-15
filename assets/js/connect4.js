417cf849c8a8752b276c0c9bf1f292e4dd351e3f
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-15 12:05:25 +0000

    add for loop for connect4

--- assets/js/connect4.js
+++ assets/js/connect4.js
@@ -3,6 +3,26 @@ const result = document.querySelector('#result');
 const displayCurrentPlayer = document.querySelector('#current-player');
 let currentPlayer = 1;
 
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+        //if the square below your current square is taken, you can go ontop of it
+        if (squares[i + 7].classList.contains('taken') && !squares[i].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+                squares[i].classList.add('taken');
+                squares[i].classList.add('player-one');
+                currentPlayer = 2;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            } else if (currentPlayer == 2) {
+                squares[i].classList.add('taken');
+                squares[i].classList.add('player-two');
+                currentPlayer = 1;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            }
+        } else alert('cant go here');
+        checkBoard();
+    }
+}
+
 const winningArrays = [
     [0, 1, 2, 3],
     [41, 40, 39, 38],
