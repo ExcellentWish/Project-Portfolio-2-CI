f093871fac3cf36471ced55d26c6e915ff3a6f7d
Nigel Doheny <nigel.doheny93@gmail.com>
2022-05-15 11:44:18 +0000

    Save variables to js and html for connect4

---
+++ assets/js/connect4.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll(".connect4-grid div");
+const result = document.querySelector('#result');
+const displayCurrentPlayer = document.querySelector('#current-player');
+let currentPlayer = 1;
